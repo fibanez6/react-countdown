545541aa694e36d195dcd1069d3641f3c674cd6b
Fernando Ibanez <Fernando.Ibanez@bjss.com>
2019-10-11 11:25:53 +0100

    updated countdown

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ class App extends Component {
         return (
             <div className="App">
                 <Countdown date="2019-11-08T20:10:00" title="Flight to Japan (11th Nov, at 20:10 UTC time):"/>
+                <Countdown date="2019-12-13T21:35:00" title="Flight to Budapest (13th Nov, at 21:35 UTC time):"/>
+                <Countdown date="2019-12-22T07:35:00" title="Flight to Madrid (22nd Nov, at 07:35 UTC time):"/>
             </div>
         );
     }
