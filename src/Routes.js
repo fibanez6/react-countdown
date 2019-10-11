a4defdbb56e82165f5b0c81ebcbbe1b74757e274
Fernando Ibanez <Fernando.Ibanez@bjss.com>
2019-10-11 09:46:01 +0100

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import App from './components/App.js';
+import history from './history';
+
+const Routes = () => {
+    return (
+        <div className="ui container">
+            <Router history={history}>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={App} />
+                        <Route path="/countdown" exact component={App} />
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
+};
+
+export default Routes;
+
